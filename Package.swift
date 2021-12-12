d4ef9b501fa7fa0cdc2850c4ae7119529b289909
a-roseanne <angelica_roseanne@yahoo.co.id>
2021-12-12 21:55:02 +0700

    able to show list but no routing

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Core",
-            targets: ["Core"]),
+            targets: ["Core"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
